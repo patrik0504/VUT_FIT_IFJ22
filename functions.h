926fe46289a7c6f07fe2a9b268f63db36857dddc
Patrik Michlian <patrik.michlian@gmail.com>
2022-11-17 14:06:16 +0100

    Add debug prints

--- functions.h
+++ functions.h
@@ -9,6 +9,20 @@
 #include <malloc.h>
 #include <math.h>
 
+
+//odkomentovat pre vypnutie debugovania
+//#define NDEBUG
+
+#ifdef NDEBUG
+#define Dprintf(FORMAT, ...) ((void)0)
+#define Dputs(MSG) ((void)0)
+#else
+#define Dprintf(FORMAT, ...) \
+    fprintf(stderr, "%s() in %s, line %i: " FORMAT "\n", \
+        __func__, __FILE__, __LINE__, __VA_ARGS__)
+#define Dputs(MSG) Dprintf("%s", MSG)
+#endif
+
 int get_string_length(char* buffer);
 
 bool is_number(char c);
